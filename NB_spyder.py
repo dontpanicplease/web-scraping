29ea05c2faa950de3e974090ce7344a5f3f48327
Anthony Lysenko <bhua2017@gmail.com>
2020-04-17 20:03:51 +0400

    17/04/20_upd

--- NB_spyder.py
+++ NB_spyder.py
@@ -8,7 +8,8 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-# LF required data.
+# Data source for scraping & required data.
+
 r = requests.get('https://www.welcomenb.ca/content/wel-bien/en/recruitment_events.html')
 soup = bs4.BeautifulSoup(r.text, "lxml")
 
@@ -20,20 +21,24 @@ for tr in maintable.find_all('tr'):
     tr_list.append(tr)
 
 
-# Turn to data frame.
+# Turn the data to dataframe.
+
 df = pd.DataFrame(data=tr_list, columns=['Date'])
 df[['Date','Time', 'Event', 'Place', 'Status']] = df['Date'].str.split('\n', expand=True)
 df.to_excel('/Users/antonlysenko/Desktop/nbsr.xlsx')
 
 
-fromaddr = "linuxman2019@gmail.com"
-toaddr = "bhua2017@gmail.com"
+# Mail setup.
+
+fromaddr = "me@gmail.com" # send from mail adr
+toaddr = "someone@gmail.com"   # send to mail adr
 
 msg = MIMEMultipart()
 
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "New Branswick upcoming National and International Recruitment Events update!"
+
+msg['Subject'] = "New Branswick upcoming National and International Recruitment Events update!" # Letter's subj
 
 body = """Hey there !
 
@@ -41,12 +46,12 @@ Please find the report about Upcoming National and International Recruitment Eve
 for NB Canada in attachment to this letter.
 
 Regards,
-Web-spider"""
+Web-spider"""  
 
 msg.attach(MIMEText(body, 'plain'))
 
 filename = "nbsr.xlsx"
-attachment = open("/Users/antonlysenko/Desktop/nbsr.xlsx", "rb")
+attachment = open("/Users/antonlysenko/Desktop/nbsr.xlsx", "rb") # file attachment path
 
 part = MIMEBase('application', 'octet-stream')
 part.set_payload((attachment).read())
@@ -57,7 +62,7 @@ msg.attach(part)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "Areyou1or0")
+server.login(fromaddr, " ")  # password for mail sender required
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
